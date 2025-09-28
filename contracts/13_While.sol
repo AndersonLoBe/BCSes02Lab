33d6124ac53ea681953984b515e85b10ba30b40b
AndersonLoBe <alopezb3@upao.edu.pe>
2025-09-27 21:27:08 -0500

    cclase5.2

--- contracts/13_While.sol
+++ contracts/13_While.sol
@@ -1,21 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
+
 import "hardhat/console.sol";
+
 contract MyWhile {
-    //sumar 'n' primeros numeros pares
-    function sumEvenNumbers(uint n) public pure returns (uint) {
+    // sumar 'n' primeros numeros pares e impares
+    //suma 'n' primeros numeros impares en otra variable 
+    //retorna ambas sumatorias
+    function sumNumbers(uint n) public pure returns (uint sumaPares, uint sumaImpares) {
         
-        uint suma = 0;
+        uint sumaPar = 0;
+        uint sumaImpar = 0;
         uint i = 1;
-        uint count = 0;
-        while (count < n) {
-            if (i % 2 == 0) {
-                console.log("Sumar: ", i);
-                suma += i;
-                count++;
+        uint countPar = 0;
+        uint countImpar = 0;
+
+        while (countPar < n || countImpar < n) {
+            if (i % 2 == 0 && countPar < n) {
+                console.log("Sumar Par: ", i);
+                sumaPar += i;
+                countPar++;
+            } else if (i % 2 != 0 && countImpar < n) {
+                console.log("Sumar Impar: ", i);
+                sumaImpar += i;
+                countImpar++;
             }
             i++;
         }
-        return suma;
+
+        return (sumaPar, sumaImpar);
     }
 }
